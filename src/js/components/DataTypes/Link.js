19d9fb862c0ca167dde4b0aeb2e3442057973224
Eyal Appelberg <eyala@nsof.io>
2019-05-26 20:44:03 +0300

    change link

--- src/js/components/DataTypes/Link.js
+++ src/js/components/DataTypes/Link.js
@@ -8,49 +8,42 @@ import Theme from './../../themes/getStyle';
 import AttributeStore from './../../stores/ObjectAttributes';
 
 export default class extends React.PureComponent {
-    constructor(props) {
-        super(props);
-        this.state = {
-            collapsed: AttributeStore.get(
-                props.rjvId,
-                props.namespace,
-                'collapsed',
-                true
-            )
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapsed: AttributeStore.get(
+        props.rjvId,
+        props.namespace,
+        'collapsed',
+        true
+      )
+    };
+  }
 
-    toggleCollapsed = () => {
-        this.setState({
-            collapsed: !this.state.collapsed
-        }, () => {
-            AttributeStore.set(
-                this.props.rjvId,
-                this.props.namespace,
-                'collapsed',
-                this.state.collapsed
-            );
-        });
-    }
+  toggleCollapsed = () => {
+    this.setState({
+      collapsed: !this.state.collapsed
+    }, () => {
+      AttributeStore.set(
+        this.props.rjvId,
+        this.props.namespace,
+        'collapsed',
+        this.state.collapsed
+      );
+    });
+  }
 
-    render() {
-        const type_name = 'link';
-        const { props } = this;
-        const { theme, value } = props;
-        let style = { style: { cursor: 'default' } };
-        const link = value.match(/href="([^"]*)/)[1];
-        const string = getTextFromHyperlink(value);
-        return (
-            <div {...Theme(theme, 'link')}>
-                <DataTypeLabel type_name={type_name} {...props} />
-                <span
-                    className="string-value"
-                    {...style}
-                    onClick={this.toggleCollapsed}
-                >
-                    <a href={link}>{string}</a>
-                </span>
-            </div>
-        );
-    }
+  render() {
+    const type_name = 'link';
+    const { props } = this;
+    const { theme, value } = props;
+    const link = value.match(/href="([^"]*)/)[1];
+    const string = getTextFromHyperlink(value);
+    return (
+      <div {...Theme(theme, 'link')}>
+        <DataTypeLabel type_name={type_name} {...props} />
+        <a href={`#/${link}`}>{string}</a>
+      </div>
+    );
+  }
 }
